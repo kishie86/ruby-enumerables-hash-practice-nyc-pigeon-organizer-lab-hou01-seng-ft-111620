a6a818b50ccdddea9771165ca6a516007b8bba39
Kishana Liburd <kishana.liburd@gmail.com>
2020-10-30 00:10:27 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,10 +4,11 @@ def nyc_pigeon_organizer(data)
   # write your code here!
   final_hash = {}
   data.each do |key, value|
-  
+    binding.pry
     value.each do |inner_key, names|
-      
+      binding.pry
       names.each do |name|
+        binding.pry
         final_hash[name] = {}
         
         
